6a23b6d8c7d18e2dd19d5db524e02549f6f616f6
DCZM17 <dczapata55@misena.edu.co>
2024-05-29 20:39:41 -0500

    Proyecto de transferencia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+import { ProductosComponent } from './productos/productos.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path: 'contact', component: ContactanosComponent},
+   {path: 'home', component: ProductosComponent},
+   {path: 'inicio', component: InicioComponent},
+   {path: '', component: ProductosComponent}
+
+];
